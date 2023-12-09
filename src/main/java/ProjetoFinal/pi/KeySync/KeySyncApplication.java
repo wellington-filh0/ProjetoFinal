fe03f1c2d3ad1c727984812b0e8f1d783060b670
wellington-filh0 <wellingtonfilho721@gmail.com>
2023-12-09 17:24:56 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/main/java/ProjetoFinal/pi/KeySync/KeySyncApplication.java
@@ -0,0 +1,13 @@
+package ProjetoFinal.pi.KeySync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeySyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeySyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjetoFinal/pi/KeySync/KeySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoFinal.pi.KeySync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
