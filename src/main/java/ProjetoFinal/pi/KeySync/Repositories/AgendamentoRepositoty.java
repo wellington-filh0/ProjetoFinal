8a58beb21e0291d3d70c6141844a7f61b91dc458
Henryguima <guimaraes.j@escolar.ifrn.edu.br>
2023-12-29 16:01:13 -0300

    Repositório de Agendamento

---
+++ src/main/java/ProjetoFinal/pi/KeySync/Repositories/AgendamentoRepositoty.java
@@ -0,0 +1,9 @@
+package ProjetoFinal.pi.KeySync.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ProjetoFinal.pi.KeySync.Models.Agendamento;
+
+public interface AgendamentoRepositoty extends JpaRepository<Agendamento,Long> {
+
+}
